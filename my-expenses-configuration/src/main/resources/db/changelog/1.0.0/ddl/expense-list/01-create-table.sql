d882fac014e922d3fddff2e89b5b47c219849818
alexasfils <afilatov@pccube.com>
2026-02-15 14:01:36 +0100

    Adedded constraint unique to expenseList name

--- my-expenses-configuration/src/main/resources/db/changelog/1.0.0/ddl/expense-list/01-create-table.sql
+++ my-expenses-configuration/src/main/resources/db/changelog/1.0.0/ddl/expense-list/01-create-table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE Expense_list (
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	name varchar(100) NOT NULL,
-	month int(12) NOT NULL,
+	month NOT NULL,
 	budget decimal(10,2) NOT NULL,
 	total_expense decimal(10,2) NOT NULL,
 	id_user BIGINT NOT NULL,
-	FOREIGN KEY (id_user) REFERENCES users (id)
+	CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES users (id),
+	CONSTRAINT uk_name_user UNIQUE (name, id_user),
+	CONSTRAINT chk_month CHECK (month BETWEEN 1 AND 12)
 );
